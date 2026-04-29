576a29830f22f87abe2aaee8e4249c58e23042ca
Charlie <32222739+ch4r13@users.noreply.github.com>
2026-04-30 00:08:58 +0200

    feat: add LVGL 8.3 config for ESP32-C3

---
+++ src/lv_conf.h
@@ -0,0 +1,101 @@
+#if 1
+
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+#include <stdint.h>
+
+#define LV_COLOR_DEPTH   16
+#define LV_COLOR_16_SWAP 1
+
+#define LV_MEM_CUSTOM 0
+#define LV_MEM_SIZE   (32U * 1024U)
+
+#define LV_DISP_DEF_REFR_PERIOD  20
+#define LV_INDEV_DEF_READ_PERIOD 30
+
+#define LV_USE_LOG 0
+
+#define LV_USE_ASSERT_NULL          0
+#define LV_USE_ASSERT_MALLOC        0
+#define LV_USE_ASSERT_STYLE         0
+#define LV_USE_ASSERT_MEM_INTEGRITY 0
+#define LV_USE_ASSERT_OBJ           0
+
+#define LV_FONT_MONTSERRAT_8  0
+#define LV_FONT_MONTSERRAT_10 0
+#define LV_FONT_MONTSERRAT_12 1
+#define LV_FONT_MONTSERRAT_14 1
+#define LV_FONT_MONTSERRAT_16 1
+#define LV_FONT_MONTSERRAT_18 0
+#define LV_FONT_MONTSERRAT_20 1
+#define LV_FONT_MONTSERRAT_22 0
+#define LV_FONT_MONTSERRAT_24 1
+#define LV_FONT_MONTSERRAT_26 0
+#define LV_FONT_MONTSERRAT_28 1
+#define LV_FONT_MONTSERRAT_30 0
+#define LV_FONT_MONTSERRAT_32 0
+#define LV_FONT_MONTSERRAT_34 0
+#define LV_FONT_MONTSERRAT_36 1
+#define LV_FONT_MONTSERRAT_38 0
+#define LV_FONT_MONTSERRAT_40 0
+#define LV_FONT_MONTSERRAT_42 0
+#define LV_FONT_MONTSERRAT_44 0
+#define LV_FONT_MONTSERRAT_46 0
+#define LV_FONT_MONTSERRAT_48 1
+#define LV_FONT_DEFAULT &lv_font_montserrat_16
+
+#define LV_USE_SUBPX 0
+
+#define LV_USE_ARC         1
+#define LV_USE_BAR         1
+#define LV_USE_BTN         1
+#define LV_USE_BTNMATRIX   0
+#define LV_USE_CANVAS      0
+#define LV_USE_CHECKBOX    0
+#define LV_USE_DROPDOWN    0
+#define LV_USE_IMG         1
+#define LV_USE_LABEL       1
+#define LV_USE_LINE        1
+#define LV_USE_ROLLER      1
+#define LV_USE_SLIDER      0
+#define LV_USE_SWITCH      1
+#define LV_USE_TEXTAREA    0
+#define LV_USE_TABLE       0
+#define LV_USE_METER       0
+
+#define LV_USE_ANIMATION   1
+#define LV_USE_FLEX        1
+#define LV_USE_GRID        0
+
+#define LV_USE_THEME_DEFAULT  1
+#define LV_THEME_DEFAULT_DARK 1
+#define LV_USE_THEME_BASIC    0
+#define LV_USE_THEME_MONO     0
+
+#define LV_IMG_CACHE_DEF_SIZE 4
+
+#define LV_USE_GPU_STM32_DMA2D  0
+#define LV_USE_GPU_NXP_PXP      0
+#define LV_USE_GPU_NXP_VG_LITE  0
+#define LV_USE_GPU_SDL          0
+
+#define LV_SPRINTF_CUSTOM    0
+#define LV_USE_USER_DATA     1
+#define LV_USE_PERF_MONITOR  0
+#define LV_USE_MEM_MONITOR   0
+#define LV_USE_REFR_DEBUG    0
+#define LV_SPRINTF_USE_FLOAT 0
+
+#define LV_USE_SNAPSHOT      0
+#define LV_USE_MONKEY        0
+#define LV_USE_GRIDNAV       0
+#define LV_USE_FRAGMENT      0
+#define LV_USE_IMGFONT       0
+#define LV_USE_MSG           0
+#define LV_USE_OBSERVER      0
+#define LV_USE_IME_PINYIN    0
+#define LV_USE_FILE_EXPLORER 0
+
+#endif
+#endif
